5936d35e61fc297dfa93070330777a3344b3b6f9
Zak Fisher <zachary.fisher@akqa.com>
2017-05-12 11:45:17 -0700

    fix onboarding

--- src/components/GuideNotesList/GuideNotesList.js
+++ src/components/GuideNotesList/GuideNotesList.js
@@ -1,20 +1,18 @@
 import React, { Component } from 'react';
-import Notes from '../../helpers/notes';
 import GuideNoteItem from '../GuideNoteItem/GuideNoteItem';
+import ONBOARDING_NOTE_IDS from '../../fixtures/onboarding';
 import './GuideNotesList.sass';
 
 class GuideNotesList extends Component {
 
   get notes() {
-    const { dataReady, user, users, notes, searchQuery, match } = this.props;
+    const { dataReady, user, users, notes, match } = this.props;
     const { noteId } = match.params;
     this.state = { activeNoteId: noteId };
     if (!dataReady || !user) return null;
 
-    // Filter notes by query
-    const filteredNotes = Notes.getNotesByQuery(users, notes, searchQuery);
-
-    return filteredNotes.map(noteId => {
+    // Filter onboarding notes
+    return ONBOARDING_NOTE_IDS.map(noteId => {
       const props = { dataReady, noteId, user, users, notes };
       return <GuideNoteItem key={noteId} active={this.state.activeNoteId === noteId} {...props} />;
     });
