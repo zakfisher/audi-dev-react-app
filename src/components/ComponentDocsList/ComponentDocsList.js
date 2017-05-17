234e9dd2ac546a80ef64f9fe3cf66bad8524b661
Kyle <kylpeacock@gmail.com>
2017-05-17 10:54:50 -0700

    CSS cleanup, stateless component syntax

--- src/components/ComponentDocsList/ComponentDocsList.js
+++ src/components/ComponentDocsList/ComponentDocsList.js
@@ -4,17 +4,11 @@ import COMPONENTS from '../../app/components';
 
 import './ComponentDocsList.sass';
 
-class ComponentDocsList extends Component {
-    
-    render(){
-        return (
-            <div className="ComponentDocsList">
-              {Object.keys(COMPONENTS).map((componentId, i) => {
-                return <ComponentDoc componentId={componentId} key={i}/>
-              })}
-            </div>
-        )
-    }
-}
-
+const ComponentDocsList = () => (
+    <div className="ComponentDocsList">
+        {Object.keys(COMPONENTS).map((componentId, i) => {
+        return <ComponentDoc componentId={componentId} key={i}/>
+        })}
+    </div>
+);
 export default ComponentDocsList;
